ba05f019f29fb5011eaa3c9020709a5aa204e8c2
@EduCCor <edd.ckalb@gmail.com>
2021-11-06 17:21:00 -0500

    open api swagger customer

--- src/main/java/ms/customer/bank/MsCustomerBankApplication.java
+++ src/main/java/ms/customer/bank/MsCustomerBankApplication.java
@@ -1,11 +1,18 @@
 package ms.customer.bank;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@OpenAPIDefinition(info = @Info(
+		title = "Ms Customer Service",
+		version = "1.0",
+		description = "documentacion de servicios para Customers"
+))
 public class MsCustomerBankApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ms/customer/bank/config/RouterConfig.java
+++ src/main/java/ms/customer/bank/config/RouterConfig.java
@@ -1,8 +1,21 @@
 package ms.customer.bank.config;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import ms.customer.bank.documents.entities.Customer;
+import ms.customer.bank.documents.entities.CustomerType;
 import ms.customer.bank.handler.CustomerHandler;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -13,6 +26,259 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class RouterConfig {
 
     @Bean
+    @RouterOperations({
+            @RouterOperation(
+                    path = "/getAll",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.GET,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "getAllCustomer",
+                    operation = @Operation(
+                            operationId = "getAllCustomer",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = Customer.class
+                                                    )
+                                            )
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/getById/{id}",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.GET,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "getByIdCustomer",
+                    operation = @Operation(
+                            operationId = "getByIdCustomer",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = Customer.class
+                                                    )
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "Customer con el id no se encontro"
+                                    )
+                            },
+                            parameters = {
+                                    @Parameter(
+                                            in = ParameterIn.PATH,
+                                            name = "id"
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/create/{code}",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.POST,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "createCustomer",
+                    operation = @Operation(
+                            operationId = "createCustomer",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = String.class
+                                                    )
+                                            )
+                                    )
+                            },
+                            parameters = {
+                                    @Parameter(
+                                            in = ParameterIn.PATH,
+                                            name = "code"
+                                    )
+                            },
+                            requestBody = @RequestBody(
+                                    content = @Content(
+                                            schema = @Schema(
+                                                implementation = Customer.class
+                                            )
+                                    )
+                            )
+                    )
+            ),
+            @RouterOperation(
+                    path = "/update/{id}",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.PUT,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "updateCustomer",
+                    operation = @Operation(
+                            operationId = "updateCustomer",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = String.class
+                                                    )
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "Customer con el id no se encontro"
+                                    )
+                            },
+                            parameters = {
+                                    @Parameter(
+                                            in = ParameterIn.PATH,
+                                            name = "id"
+                                    )
+                            },
+                            requestBody = @RequestBody(
+                                    content = @Content(
+                                            schema = @Schema(
+                                                    implementation = Customer.class
+                                            )
+                                    )
+                            )
+                    )
+            ),
+            @RouterOperation(
+                    path = "/delete/{id}",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.DELETE,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "deleteCustomer",
+                    operation = @Operation(
+                            operationId = "deleteCustomer",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = String.class
+                                                    )
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "Customer con el id no se encontro"
+                                    )
+                            },
+                            parameters = {
+                                    @Parameter(
+                                            in = ParameterIn.PATH,
+                                            name = "id"
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/customerType/getAll",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.GET,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "getAllCustomerType",
+                    operation = @Operation(
+                            operationId = "getAllCustomerType",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = CustomerType.class
+                                                    )
+                                            )
+                                    )
+                            }
+                    )
+            ),
+            @RouterOperation(
+                    path = "/customerType/create",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.POST,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "createCustomerType",
+                    operation = @Operation(
+                            operationId = "createCustomerType",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = String.class
+                                                    )
+                                            )
+                                    )
+                            },
+                            requestBody = @RequestBody(
+                                    content = @Content(
+                                            schema = @Schema(
+                                                    implementation = CustomerType.class
+                                            )
+                                    )
+                            )
+                    )
+            ),
+            @RouterOperation(
+                    path = "/customerType/delete/{id}",
+                    produces = {
+                            MediaType.APPLICATION_JSON_VALUE
+                    },
+                    method = RequestMethod.DELETE,
+                    beanClass = CustomerHandler.class,
+                    beanMethod = "deleteCustomerType",
+                    operation = @Operation(
+                            operationId = "deleteCustomerType",
+                            responses = {
+                                    @ApiResponse(
+                                            responseCode = "200",
+                                            description = "Operación Satisfactoria",
+                                            content = @Content(
+                                                    schema = @Schema(
+                                                            implementation = String.class
+                                                    )
+                                            )
+                                    ),
+                                    @ApiResponse(
+                                            responseCode = "404",
+                                            description = "CustomerType con el id no se encontro"
+                                    )
+                            },
+                            parameters = {
+                                    @Parameter(
+                                            in = ParameterIn.PATH,
+                                            name = "id"
+                                    )
+                            }
+                    )
+            )
+    })
     public RouterFunction<ServerResponse> routes(CustomerHandler customerHandler){
         return route(GET("/getAll"), customerHandler::getAllCustomer)
                 .andRoute(GET("/getById/{id}"), customerHandler::getByIdCustomer)
